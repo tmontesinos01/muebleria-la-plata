94dd19961ec86bcc5a64ef68859157d63e3074d0
montesinost01 <montesinost01@gmail.com>
2025-09-04 14:52:07 +0000

    Subiendo estado completo del proyecto

--- Data/obj/Debug/net9.0/Data.AssemblyInfo.cs
+++ Data/obj/Debug/net9.0/Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c37893f9cf113b50a4c03f1615e4e92aaac21b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fb9c71855ba3bf7d9b52e6073f177a395800c23")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
